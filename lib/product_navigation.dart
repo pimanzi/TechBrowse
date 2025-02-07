5301ea5f2201107792cbdc2658f62bc16348bec7
Placide Imanzi <141280521+pimanzi@users.noreply.github.com>
2025-02-07 18:11:58 +0200

    Create product_navigation.dart

---
+++ lib/product_navigation.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:techbrowse/product_card.dart';
+
+class ProductNavigation extends StatelessWidget {
+  ProductNavigation({super.key});
+
+  final List<Map<String, dynamic>> productInfo = [
+    {
+      "title": "Pixel 1",
+      "title2": "Pixel",
+      "description": "Pixel is the most featureful phone ever",
+      "price": 800,
+      "fillStar": false,
+      "color": Color(0xFF3C64DC),
+    },
+    {
+      "title": "Laptop",
+      "title2": "Laptop",
+      "description": "Laptop is the most productive development tool",
+      "price": 2000,
+      "fillStar": false,
+      "color": Colors.green,
+    },
+    {
+      "title": "Tablet",
+      "title2": "Tablet",
+      "description": "Tablet is the most useful device ever for meetings",
+      "price": 1500,
+      "fillStar": true,
+      "color": Color(0xFFcfc534),
+    },
+    {
+      "title": "Pendrive",
+      "title2": "Pendrive",
+      "description": "Pendrive is a compact and portable storage device",
+      "price": 100,
+      "fillStar": false,
+      "color": Color(0xFFc45c3f),
+    },
+    {
+      "title": "Floppy Drive",
+      "title2": "Floppy Drive",
+      "description": "Old but still gold storage device",
+      "price": 50,
+      "fillStar": false,
+      "color": Color(0xFF3cbcac),
+    },
+  ];
